05594acee0d33422058e79b6181626842eb7cf5b
ryo chigira <c.ryo.09@icloud.com>
2022-08-22 19:37:15 +0900

    1.0

---
+++ hick_kill/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace hick_kill
+{
+    public partial class Form1 : Form
+    {
+        NotifyIcon notifyIcon;
+        public Form1()
+        {
+            this.ShowInTaskbar = false;
+            this.setComponents();
+        }
+
+
+        private void setComponents()
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.kill;
+            notifyIcon.Visible = true;
+            notifyIcon.Text = "";
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            ToolStripMenuItem toolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem.Text = "&Quit";
+            toolStripMenuItem.Click += ToolStripMenuItem_Click;
+            contextMenuStrip.Items.Add(toolStripMenuItem);
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+            notifyIcon.MouseDoubleClick += NotifyIcon_Click;
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcessesByName("hick");
+            foreach (System.Diagnostics.Process p in ps)
+            {
+                p.Kill();
+            }
+        }
+
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ hick_kill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hick_kill
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            new Form1();
+            Application.Run();
+        }
+    }
+}
